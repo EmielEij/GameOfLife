f1a6629730ccc29b10bbe9cffd16d98d31a1f622
Emiel Eij <373939@student.fontys.nl>
2021-06-15 12:53:30 +0200

    matrix added

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,5 +1,6 @@
 set(sources
     src/tmp.cpp
+    src/Matrix.cpp
 )
 
 set(exe_sources
@@ -9,5 +10,5 @@ set(exe_sources
 
 set(headers
     include/project/tmp.hpp
-    include/GameOfLife
+    include/GameOfLife/Matrix.h
 )
---
+++ src/Matrix.cpp
@@ -0,0 +1,2 @@
+#include "GameOflife/Matrix.h"
+
